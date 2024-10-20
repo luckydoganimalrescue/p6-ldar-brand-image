800afc1f3de21e182ebc9cddd750843d40f4c444
Philip M. Gollucci <pgollucci@p6m7g8.com>
2024-10-20 11:41:22 -0400

    runs

---
+++ api/TODO.md
@@ -0,0 +1,2 @@
+aws-lambda has no replacement for @aws-sdk v3 which is EOL
+sharp breaks sam with x86_64 for brand
--- api/esbuild.config.mjs
+++ api/esbuild.config.mjs
@@ -2,13 +2,13 @@ import * as process from 'node:process'
 import esbuild from 'esbuild'
 
 esbuild.build({
-  entryPoints: ['./api/presign.ts', './api/brand.ts'],
+  entryPoints: ['./src/presign.ts', './src/brand.ts'],
   bundle: true,
   platform: 'node',
   target: ['node20'],
   outdir: './dist',
   sourcemap: true,
-  minify: true,
+  minify: false,
   external: ['aws-sdk'],
   logLevel: 'info',
 }).catch(() => process.exit(1))
