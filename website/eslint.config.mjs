41434050ae836e64ab2a9b40093b2a07da52f04b
Philip M. Gollucci <pgollucci@p6m7g8.com>
2024-10-20 09:21:24 -0400

    Runs

--- website/eslint.config.mjs
+++ website/eslint.config.mjs
@@ -1,5 +1,5 @@
 import antfu from '@antfu/eslint-config'
-import nextPlugin from '@next/eslint-plugin-next'
+import next from '@next/eslint-plugin-next'
 import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import tailwindcss from 'eslint-plugin-tailwindcss'
@@ -19,7 +19,7 @@ export default antfu({
   plugins: {
     react,
     'react-hooks': reactHooks,
-    '@next/eslint-plugin-next': nextPlugin,
+    next,
     tailwindcss,
   },
   languageOptions: {
@@ -34,8 +34,8 @@ export default antfu({
     'react/react-in-jsx-scope': 'off',
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'error',
-    // '@next/next/no-img-element': 'error',
-    // '@next/next/no-page-custom-font': 'error',
+    // 'next/no-img-element': 'error',
+    // 'next/no-page-custom-font': 'error',
     'tailwindcss/classnames-order': 'error',
   },
   settings: {
